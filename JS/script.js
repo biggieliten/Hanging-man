07b6f08129b1202c746e4b08b83f28683a92bf38
CrnaStrela94 <luka940114@gmail.com>
2023-10-10 15:20:51 +0200

    update random word and checker

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,39 @@
+/////////////////////Luka Random Generator ///////////////////////////////
 let randomWord = ["apple", "banana", "cherry", "grape", "orange", "pear"];
 
+function randomWordGenerator(randomWord) {
+  return randomWord[Math.floor(Math.random() * randomWord.length)];
+}
+const randomWordChosen = randomWordGenerator(randomWord);
+console.log(randomWordChosen);
+const randomWordChosenArray = randomWordChosen.split(``);
+console.log(randomWordChosenArray);
+///////////////////////////check letter Luka////////////////////////////////
 let inputLetter = document.querySelector(`#letter1`);
 let inputWord = document.querySelector(`#letter1`);
 let result = document.querySelector(`#result`);
 
-inputLetter.addEventListener(`input`, () => {
-  inputWord.value += inputLetter.value;
-  result.textContent = inputWord.value;
-  if (inputWord.value.length === randomWord.length) {
-    inputWord.value = "";
-    result.textContent = "";
-    alert("You Win!");
-    inputLetter.value = "";
-    inputWord.value = "";
-    result.textContent = "";
-    return;
+inputLetter.addEventListener(`input`, (checkLetter) => {
+  let letter = checkLetter.target.value.toLowerCase();
+  if (randomWordChosenArray.includes(letter)) {
+    for (let i = 0; i < randomWordChosenArray.length; i++) {
+      if (letter === randomWordChosenArray[i]) {
+        console.log("Correct!");
+      }
+      console.log(randomWordChosenArray[i]);
+    }
+  } else {
+    console.log("Incorrect!");
   }
 });
+/////////////////////////output lines with amount of letters Luka //////////////////
+
+// function outputLines(randomWordChosenArray) {
+//   for (let i = 0; i < randomWordChosenArray.length; i++) {
+//     result.innerHTML += randomWordChosenArray[i] + `-`;
+//   }
+// }
+
+// outputLines(randomWordChosenArray);
+let paragraph = document.querySelector(`#outputLines`);
+paragraph.innerHTML = `${randomWordChosenArray[i]}`;
